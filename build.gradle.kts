a75c86e281f66b13dbc2868c8c8cf1ee1850c52b
Dilip Patel <pateldilip2902@gmail.com>
2024-10-18 18:44:17 +0530

    added spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -18,15 +21,44 @@ plugins {
     alias(libs.plugins.spotless) apply false
 }
 
-//// Spotless configuration
-//subprojects {
-//    apply(plugin = "com.diffplug.spotless")
-//
-//    spotless {
-//        kotlin {
-//            target("**/*.kt")
-//            // Add formatting options as needed
-//            ktlint()
+val ktlintVersion = libs.versions.ktlint.get()
+
+// Spotless configuration
+subprojects {
+    apply<SpotlessPlugin>()
+    configure<SpotlessExtension> {
+
+        kotlin {
+            target("**/*.kt", "**/*.kts")
+            targetExclude("**/build/**/*.kt")
+            ktlint(ktlintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
+                        "ktlint_standard_class-signature" to "disabled",
+                        "ktlint_standard_discouraged-comment-location" to "disabled",
+                        "ktlint_standard_function-expression-body" to "disabled",
+                        "ktlint_standard_function-signature" to "disabled",
+                        "ktlint_standard_max-line-length" to "disabled",
+                        "ktlint_standard_type-argument-comment" to "disabled",
+                        "ktlint_standard_value-argument-comment" to "disabled",
+                        "ktlint_standard_value-parameter-comment" to "disabled",
+                    )
+                )
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        kotlinGradle {
+            target("src/**/*.kt")
+            ktlint(ktlintVersion)
+        }
+
+//        afterEvaluate {
+//            tasks.withType<KotlinCompile>() {
+//                finalizedBy("spotlessApply")
+//            }
 //        }
-//    }
-//}
+
+    }
+}
